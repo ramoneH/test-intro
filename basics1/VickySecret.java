395525509c3884139a3b9ca90d763152a27cf97d
javaclass <javaclass@15000533cpu.fec.org>
2017-09-26 11:30:41 -0500

    This is a program for women.

---
+++ basics1/VickySecret.java
@@ -0,0 +1,11 @@
+package basics1;
+
+public class VickySecret {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Great Day For Shopping");
+
+	}
+
+}
