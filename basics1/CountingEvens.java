d9e5d30066f60ac26b696cf5d500018531f8ffb3
javaclass <javaclass@15000533cpu.fec.org>
2017-10-06 12:40:32 -0500

    ANother Push

---
+++ basics1/CountingEvens.java
@@ -0,0 +1,34 @@
+package basics1;
+
+import java.util.Scanner;
+
+public class CountingEvens {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] num = {1,8,4,66,4,5,51,1,0};
+		int evenNum = 0;
+		System.out.println(num);
+		System.out.println("I'll Show Your Even Number Count");
+		//int n = new Scanner(System.in).nextInt();
+		
+		
+		for (int i=0;i<num.length;i++) {
+			System.out.println(num);
+			System.out.println(num);
+			if (num[i]==2
+			|| num[i]==4 ){
+				
+				evenNum++;
+			}
+			
+			System.out.println(evenNum);
+			
+			
+		{
+			
+
+		}
+	}
+	}
+}
